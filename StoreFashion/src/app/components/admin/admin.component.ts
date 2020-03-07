db1349a6e375aac39d93fe573ef190f0ea47226d
Matatov1989 <Matatov1989@gmail.com>
2020-03-07 19:23:22 +0200

    07.03.2020

--- StoreFashion/src/app/components/admin/admin.component.ts
+++ StoreFashion/src/app/components/admin/admin.component.ts
@@ -3,7 +3,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { finalize, tap } from 'rxjs/operators';
+import { map, finalize } from 'rxjs/operators';
 
 import * as firebase from 'firebase';
 
@@ -22,46 +22,13 @@ export class AdminComponent implements OnInit {
   ];
 
   product: Product;
-
- file: File;
+  links: any[] = [];
 
   task: AngularFireUploadTask;
 
   percentage: Observable<number>;
   snapshot: Observable<any>;
   downloadURL;
-
-  constructor(private firestore: AngularFirestore, private storage: AngularFireStorage) { }
-
-  ngOnInit(): void {
-    	console.log("ngOnInit ");
-//     this.startUpload();
-  }
-
-  startUpload() {
-
-    // The storage path
-    const path = `test/${Date.now()}_${this.file.name}`;
-    console.log("path ", path);
-    // Reference to storage bucket
-    const ref = this.storage.ref(path);
-
-    // The main task
-    this.task = this.storage.upload(path, this.file);
-
-    // Progress monitoring
-    this.percentage = this.task.percentageChanges();
-
-    this.snapshot   = this.task.snapshotChanges().pipe(
-      tap(console.log),
-      // The file's download URL
-      finalize( async() =>  {
-        this.downloadURL = await ref.getDownloadURL().toPromise();
-
-        this.firestore.collection('files').add( { downloadURL: this.downloadURL, path });
-      }),
-    );
-  }
 
   profileForm = new FormGroup({
     formProductName: new FormControl(''),
@@ -73,8 +40,38 @@ export class AdminComponent implements OnInit {
 
   files: File[] = [];
 
+  constructor(private firestore: AngularFirestore, private storage: AngularFireStorage) { }
+
+  ngOnInit(): void {
+    	console.log("ngOnInit ");
+  }
+
+  startUpload() {
+
+    for (let i = 0; i < this.files.length; i++) {
+
+      // The storage path
+      const path = `test/${Date.now()}_123`;
+      // Reference to storage bucket
+      const ref = this.storage.ref(path);
+
+      this.storage.upload(path, this.files[i])
+      .snapshotChanges().pipe(
+        finalize(() => {
+          ref.getDownloadURL().subscribe((url) => {
+            console.log("url  ", url);
+      //      this.firestore.collection('files').add( { downloadURL: url, path });
+            this.links.push(url);
+
+          })
+        })
+      ).subscribe();
+    }
+  }
+
 	onSelect(event) {
-		console.log("dron zone select ", event);
+		console.log("dron zone select EVENT ", event.addedFiles);
+
 		this.files.push(...event.addedFiles);
 
     this.startUpload();
@@ -89,10 +86,6 @@ export class AdminComponent implements OnInit {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
 
-    console.log('FORM ', this.profileForm.value.formProductName);
-
-    console.log('select ',  this.profileForm.value.formProductSell);
-
     this.product = {
       productId: "1234567",
       productName: this.profileForm.value.formProductName,
@@ -101,14 +94,14 @@ export class AdminComponent implements OnInit {
       productDescription: this.profileForm.value.formProductDescription,
       productInsertdDate: firebase.firestore.FieldValue.serverTimestamp(),
       productSell: this.profileForm.value.formProductSell,
-      productPictures: "",
+      productImages: this.links,
     }
-
+    console.log('this.product ', this.product);
     this.createPolicy(this.product);
   }
 
   createPolicy(product: Product){
-    return this.firestore.collection('Product').add(product);
+    return this.firestore.collection('Product').add(this.product);
   }
 /*
   updatePolicy(product: Product){
@@ -138,5 +131,5 @@ interface Product {
   productDescription: string,
   productInsertdDate: any,
   productSell: boolean,
-  productPictures: any,
+  productImages: Array<string>;
 }
--- StoreFashion/src/app/components/home/home.component.ts
+++ StoreFashion/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 
 @Component({
@@ -8,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title = 'Store Fashion';
+
+  product: Product[] = [];
+
+  prod: string[] = [];
 
   listSortPrice: SortPrice[] = [
     {value: 'steak-0', viewValue: 'по убыванию'},
@@ -20,9 +25,64 @@ export class HomeComponent implements OnInit {
     {value: 'tacos-2', viewValue: 'Tacos'}
   ];
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.getProducts1();
+  //            console.log("data  ", this.getProducts());
+  }
+
+  getProducts1() {
+     this.firestore.collection('Product')
+      .snapshotChanges().subscribe(serverItems => {
+      //  this.users = [];
+        serverItems.forEach(docProduct => {
+
+          let product: any = docProduct.payload.doc.data();
+
+          console.log('Constractor user ', product)
+
+          this.product.push(
+          {
+            productId: product.productId.value,
+            productName: product.productName.value,
+            productPrice: product.productPrice.value,
+            productType: product.productType.value,
+            productDescription: product.productDescription.value,
+            productInsertdDate: product.productInsertdDate.value,
+            productSell: product.productSell.value,
+            productImages: product.productImages.value,
+          })
+
+            console.log('Constractor size ', this.product[0].productName);
+        });
+      });
+
+
+    }
+
+  getProducts() {
+    return this.firestore.collection('Product').valueChanges()
+    .subscribe(val =>
+//      console.log("val  ", val.length)
+
+      this.prod  = Object.assign([], val)
+/*
+      this.product.push(
+      {
+        productId: val.productId.value,
+        productName: val.productName.value,
+        productPrice: val.productPrice.value,
+        productType: val.productType.value,
+        productDescription: val.productDescription.value,
+        productInsertdDate: val.productInsertdDate.value,
+        productSell: val.productSell.value,
+        productImages: val.productImages.value,
+      })
+      */
+    );
+
+    console.log("product  ", this.prod.length);
   }
 
 }
@@ -35,4 +95,16 @@ interface SortPrice {
 interface SortTop {
   value: string;
   viewValue: string;
+}
+
+
+interface Product {
+  productId: string,
+  productName: string,
+  productPrice: number,
+  productType: string,
+  productDescription: string,
+  productInsertdDate: any,
+  productSell: boolean,
+  productImages: Array<string>;
 }
